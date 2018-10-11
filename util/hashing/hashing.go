40d2b1ab93b2342b4dd95cfae00ab25e64b9957e
Micha Spahr <mic.spahr.16@lehre.mosbach.dhbw.de>
2018-10-11 10:47:01 +0200

    changed folder structure

---
+++ util/hashing/hashing.go
@@ -0,0 +1,20 @@
+package hashing
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+// CheckPassword compares the given password against the stored hash.
+// It returns true, if the password is correct.
+func CheckPassword(hash string, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+// GenerateHash returns a bcrypt hash for the given password
+// Bcrypt is a very secure hashing algorithm for passwords, which
+// also spares the developer of having to generate a salt on his/her own
+func GenerateHash(password string) (string, error) {
+	hash, error := bcrypt.GenerateFromPassword([]byte(password), 12)
+	return string(hash), error
+}
