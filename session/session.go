b899ad0a72c34ed9b5867fe27b4ab059277095fe
Micha Spahr <mic.spahr.16@lehre.mosbach.dhbw.de>
2018-11-12 16:30:59 +0100

    Refactored session

---
+++ session/session.go
@@ -0,0 +1,145 @@
+package session
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"io"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/mortenterhart/trivial-tickets/globals"
+	"github.com/mortenterhart/trivial-tickets/structs"
+)
+
+/*
+*
+* Matrikelnummern
+* 3040018
+* 6694964
+* 3478222
+ */
+
+func CreateSession(sessionId string) structs.SessionManager {
+
+	session := structs.Session{
+		User:       structs.User{},
+		CreateTime: time.Now(),
+		IsLoggedIn: false,
+		Id:         sessionId,
+	}
+
+	return structs.SessionManager{
+		Name:    sessionId,
+		Session: session,
+		TTL:     3600,
+	}
+}
+
+func GetSession(sessionId string) (structs.Session, error) {
+
+	session := globals.Sessions[sessionId].Session
+
+	if session != (structs.Session{}) {
+		return globals.Sessions[sessionId].Session, nil
+	} else {
+		return structs.Session{}, errors.New("Unable to find session with id: " + sessionId)
+	}
+}
+
+func UpdateSession(sessionId string, session structs.Session) {
+
+	globals.Sessions[sessionId] = structs.SessionManager{
+		Name:    sessionId,
+		Session: session,
+		TTL:     3600,
+	}
+}
+
+func CreateSessionId() (string, error) {
+
+	const length = 32
+
+	sessionId := make([]byte, length)
+
+	l, errRnd := io.ReadFull(rand.Reader, sessionId)
+
+	if errRnd != nil && l == length {
+		return "", errors.New("Unable to create session id")
+	}
+
+	return base64.URLEncoding.EncodeToString(sessionId), nil
+}
+
+// createSessionCookie returns a http cookie to hold the session
+// id for the user
+func CreateSessionCookie() (*http.Cookie, string, error) {
+
+	sessionId, errSessionId := CreateSessionId()
+
+	if errSessionId != nil {
+		return nil, "", errors.New("Unable to create session id")
+	}
+
+	return &http.Cookie{
+		Name:     "session",
+		Value:    sessionId,
+		HttpOnly: false,
+		Expires:  time.Now().Add(2 * time.Hour)}, sessionId, nil
+}
+
+// deleteSessionCookie returns a http cookie which will overwrite the
+// existing session cookie in order to nulify it
+func DeleteSessionCookie() *http.Cookie {
+
+	return &http.Cookie{
+		Name:     "session",
+		Value:    "",
+		HttpOnly: false,
+		Expires:  time.Now().Add(-100 * time.Hour)}
+}
+
+// getSessionId retrieves the session id from the cookie of the user
+func GetSessionId(r *http.Request) string {
+
+	// Get the cookie with the session id
+	userCookie, errUserCookie := r.Cookie("session")
+
+	if errUserCookie != nil {
+		log.Print(errUserCookie)
+		return errUserCookie.Error()
+	}
+
+	return userCookie.Value
+}
+
+// checkForSession either returns a new session or the existing session of a user.
+func CheckForSession(w http.ResponseWriter, r *http.Request) (structs.Session, error) {
+
+	var newSession structs.Session
+
+	// Check if the user already has a session
+	// If not, create one
+	// Otherwise read the session id and load the index with his session
+	if _, err := r.Cookie("session"); err != nil {
+
+		cookie, sessionId, errCreateSessionCookie := CreateSessionCookie()
+
+		if errCreateSessionCookie != nil {
+			return structs.Session{}, errors.New("Unable to create a session cookie")
+		}
+
+		http.SetCookie(w, cookie)
+		globals.Sessions[sessionId] = CreateSession(sessionId)
+
+		newSession = globals.Sessions[sessionId].Session
+
+	} else {
+		sessionId := GetSessionId(r)
+
+		newSession = globals.Sessions[sessionId].Session
+	}
+
+	return newSession, nil
+}
---
+++ session/session_test.go
@@ -0,0 +1,99 @@
+package session
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/mortenterhart/trivial-tickets/globals"
+	"github.com/stretchr/testify/assert"
+)
+
+// Testing the various session functions, combined for easier testing
+func TestSessionFunctions(t *testing.T) {
+
+	// Test CreateSessionCookie
+	_, sessionId, errCreateSessionCookie := CreateSessionCookie()
+
+	assert.Nil(t, errCreateSessionCookie, "There was an error creating the cookie")
+
+	// Test CreateSession
+	session1 := CreateSession(sessionId)
+
+	assert.Equal(t, session1.Session.Id, sessionId, "The session id was not used to create the session")
+
+	// Test GetSession
+	session2, errGetSession := GetSession(sessionId)
+
+	assert.NotNil(t, session2, "Session was not created")
+	assert.NotNil(t, errGetSession, "No error was returned, although the id does not exist")
+
+	globals.Sessions[sessionId] = session1
+
+	session3, errGetSession3 := GetSession(sessionId)
+
+	assert.Equal(t, session3.Id, sessionId, "The session id was not used to create the session")
+	assert.Nil(t, errGetSession3, "An error was returned, but the session was available")
+
+	// Test UpdateSession
+	session3.IsLoggedIn = true
+
+	UpdateSession(sessionId, session3)
+
+	assert.True(t, session3.IsLoggedIn, "Struct value was not changed")
+
+	// Test CheckForSession
+	cookie, _, _ := CreateSessionCookie()
+	rr := httptest.NewRecorder()
+	http.SetCookie(rr, cookie)
+	request := &http.Request{Header: http.Header{"Cookie": rr.HeaderMap["Set-Cookie"]}}
+
+	rr2 := httptest.NewRecorder()
+	session4, errCheckForSession := CheckForSession(rr, request)
+	session5, _ := CheckForSession(httptest.NewRecorder(), &http.Request{Header: http.Header{"Cookie": rr2.HeaderMap["Set-Cookie"]}})
+
+	assert.NotNil(t, session4, "The session was not created correctly")
+	assert.True(t, !session4.User.IsOnHoliday, "The session was not created correctly")
+	assert.Nil(t, errCheckForSession, "There was an error, although the request was valid")
+	assert.NotNil(t, session5, "The session without the cookie was not created properly")
+
+}
+
+// TestCookieFunctions tests all cookie related functions including errors
+func TestCookieFunctions(t *testing.T) {
+
+	// Test creating a new session cookie
+	cookie, sessionId, errCreateSessionCookie := CreateSessionCookie()
+
+	assert.Nil(t, errCreateSessionCookie, "The cookie could not be created")
+	assert.NotNil(t, sessionId, "The returned session is was nil")
+	assert.NotNil(t, cookie, "The returned cookie is was nil")
+	assert.Equal(t, cookie.Name, "session", "The cookie was not named session")
+	assert.True(t, (len(sessionId) == 44), "The session is has the wrong length")
+
+	// Test to retrieve the session id from a set cookie
+	rr := httptest.NewRecorder()
+	http.SetCookie(rr, cookie)
+	request := &http.Request{Header: http.Header{"Cookie": rr.HeaderMap["Set-Cookie"]}}
+	sId := GetSessionId(request)
+
+	assert.NotNil(t, sId, "No session id was found")
+	assert.True(t, (len(sId) == 44), "Session id has the wrong length")
+
+	// Test that an error string is returned, when there is no cookie
+	request2 := &http.Request{}
+	sId2 := GetSessionId(request2)
+
+	assert.False(t, (len(sId2) == 44), "No error was returned")
+
+	// Overwrite the session cookie, so that it is deleted
+	cookie2 := DeleteSessionCookie()
+
+	assert.NotNil(t, cookie2, "Cookie was not overwritten")
+	assert.Equal(t, cookie2.Value, "", "Value of cookie was not emptied")
+}
+
+// TestCheckForSession creates a request to test the creation of a session for a user
+func TestCheckForSession(t *testing.T) {
+
+}
