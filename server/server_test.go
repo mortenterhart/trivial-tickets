38f784cca4bc50fb45f434ecc99334bbf31f1ed9
Micha Spahr <mic.spahr.16@lehre.mosbach.dhbw.de>
2018-11-22 11:55:26 +0100

    Added unit tests to server.go

--- server/server_test.go
+++ server/server_test.go
@@ -1,8 +1,11 @@
 package server
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/mortenterhart/trivial-tickets/structs"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -23,4 +26,55 @@ func TestGetTemplates(t *testing.T) {
 
 	assert.NotNil(t, tmpl, "GetTemplates() returned no found templates")
 	assert.Nil(t, tmplNil, "GetTemplates() found templates where it was not supposed to be")
+}
+
+func TestRedirectToTLS(t *testing.T) {
+
+	req, _ := http.NewRequest("GET", "localhost", nil)
+	w := httptest.NewRecorder()
+	handler := http.HandlerFunc(redirectToTLS)
+
+	handler.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusTemporaryRedirect, w.Code, "The HTTP status code was incorrect")
+}
+
+func TestRedirectToTLSWithParams(t *testing.T) {
+
+	req, _ := http.NewRequest("GET", "localhost?id=123", nil)
+	w := httptest.NewRecorder()
+	handler := http.HandlerFunc(redirectToTLS)
+
+	handler.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusTemporaryRedirect, w.Code, "The HTTP status code was incorrect")
+}
+
+func TestStartHandlers(t *testing.T) {
+
+	err := startHandlers("../www")
+
+	assert.Nil(t, err, "An error occured, although the path was correct")
+}
+
+func TestStartHandlersNoPath(t *testing.T) {
+
+	err := startHandlers("")
+
+	assert.NotNil(t, err, "No error occured, although the path was incorrect")
+}
+
+func TestStartServer(t *testing.T) {
+
+}
+
+func mockConfig() structs.Config {
+
+	return structs.Config{
+		Port:    443,
+		Tickets: "../files/tickets",
+		Users:   "../files/users/users.json",
+		Cert:    "../ssl/server.cert",
+		Key:     "../ssl/server.key",
+		Web:     "../www"}
 }
