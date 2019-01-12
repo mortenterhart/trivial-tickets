86c6d93c630db90b124bdb43a3988c052558f32c
Morten Terhart <32468043+mortenterhart@users.noreply.github.com>
2019-01-12 03:00:43 +0100

    Extended startup scripts to install Go dependencies

--- startServer.bat
+++ startServer.bat
@@ -1,5 +1,34 @@
-CD cmd/ticketsystem
+@echo off
 
-go run ticketsystem.go
+set server_name=trivial-tickets
+set dirname=%~dp0
 
-CD ../..
+IF EXIST "cmd\ticketsystem" (
+    call :info Checking for missing Go dependencies
+    go get -t -v ./...
+
+    IF %ERRORLEVEL% == 0 (
+        cd cmd\ticketsystem
+
+        call :info Starting Ticketsystem webserver
+        go run ticketsystem.go
+
+        cd ../..
+    )
+) ELSE (
+    call :error cannot find the main executable cmd\ticketsystem\ticketsystem.go
+    echo You might be in the wrong working directory, execute
+    echo   cd %dirname%
+    echo to change to the correct directory.
+    exit /B 1
+)
+
+exit /B 0
+
+:info
+    echo [%server_name%] INFO %*
+exit /B 0
+
+:error
+    echo [%server_name%] ERROR %*
+exit /B 0
